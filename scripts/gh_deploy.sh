9f2b72ce68343b8dcd5585b4a03054ef7d4622fa
hyeon8571 <w01085914442@gmail.com>
2023-11-14 12:25:24 +0900

    script 파일 수정

--- scripts/gh_deploy.sh
+++ scripts/gh_deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 PROJECT_NAME="tomato_disease_analyzer"
-JAR_PATH="/home/ubuntu/tomato_disease_analyzer/build/libs/*.jar"
+JAR_PATH="/home/ubuntu/springboot-CI-CD/tomato_disease_analyzer/build/libs/*.jar"
 DEPLOY_PATH=/home/ubuntu/$PROJECT_NAME/
-DEPLOY_LOG_PATH="/home/ubuntu/$PROJECT_NAME/deploy.log"
-DEPLOY_ERR_LOG_PATH="/home/ubuntu/$PROJECT_NAME/deploy_err.log"
-APPLICATION_LOG_PATH="/home/ubuntu/$PROJECT_NAME/application.log"
+DEPLOY_LOG_PATH="/home/ubuntu/springboot-CI-CD/$PROJECT_NAME/deploy.log"
+DEPLOY_ERR_LOG_PATH="/home/ubuntu/springboot-CI-CD/$PROJECT_NAME/deploy_err.log"
+APPLICATION_LOG_PATH="/home/ubuntu/springboot-CI-CD/$PROJECT_NAME/application.log"
 BUILD_JAR=$(ls $JAR_PATH)
 JAR_NAME=$(basename $BUILD_JAR)
 
